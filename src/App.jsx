d05d2e50214b629f472a65145e9b464e463acce2
dimitri_atom <scchooyi@gmail.com>
2026-01-11 17:00:42 +0900

    6.4 Cleanup code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+
+function Hello() {
+    useEffect(() => {
+        console.log("hi :)");
+        return () => console.log("bye :(");
+    }, []);
+    return <h1>Hello</h1>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          {showing ? <Hello /> : null}
+          <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
