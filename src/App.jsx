ee8b95054855c340f73cf1a751ca16ba221936e5
dimitri_atom <scchooyi@gmail.com>
2026-01-13 12:09:40 +0900

    7.4 Movie App part Two

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import Movie from "./components/Movie.jsx";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                'https://yts.lt/api/v2/list_movies.json?sort_by=year'
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getMovies();
-    }, []);
-
-    console.log(movies);
-
-    return (
-        <div>
-            {loading ? (<h1>Loading...</h1>) :
-                (
-                    <div>
-                        {movies.map((movie) => (
-                            <div key={movie.id}>
-                                <img src={movie.medium_cover_image} />
-                                <h2>{movie.title}</h2>
-                                <ul>
-                                    {movie.genres.map((g) => (
-                                        <li key={g}>{g}</li>
-                                    ))}
-                                    <hr/>
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
-                )}
-        </div>
-    );
+    return null;
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청을 'https://yts.lt'로 전달합니다.
+      '/api': {
+        target: 'https://yts.lt',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
